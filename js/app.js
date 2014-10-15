a0e35112376befb1ef47d34c76f8a4a782307cb2
Emnet Gossaye <emnetg@uw.edu>
2014-10-15 12:19:23 -0700

    in class work javascript forms

--- js/app.js
+++ js/app.js
@@ -11,7 +11,42 @@
 * and add an event listener for the form's submit event
 * */
 function onReady() {
-    var standings = ['Freshman', 'Sophomore', 'Junior', 'Senior', 'Super Senior!'];
+    var standings = [
+        {
+            code: 'f',
+            displayText: 'Freshman'
+        },
+        {
+            code: 's',
+            displayText: 'Sophomore'
+        },
+        {
+            code: 'j',
+            displayText: 'Junior'
+        },
+        {
+            code: 'sn',
+            displayText: 'Senior'
+        },
+
+        {   code: 'ss',
+            displayText: 'Super Senior!'
+        }
+    ];
+    var personForm = document.getElementById('person-form');
+
+    var standingsSelect = personForm.elements['standing'];
+    var idx;
+    var option;
+
+    for (idx = 0; idx < standings.length; ++idx) {
+        option = document.createElement('option');
+        option.innerHTML = standings[idx].displayText;
+        option.value = standings[idx].code;
+        standingsSelect.appendChild(option);
+    }
+
+    personForm.addEventListener('submit', onSubmit);
 
 } //onReady()
 
@@ -23,6 +58,9 @@ function onReady() {
  * */
 function onSubmit(evt) {
     evt.returnValue = validateForm(this);
+    if(!evt.returnValue && evt.preventDefault) {
+        evt.preventDefault();
+    }
     return evt.returnValue;
 } //onSubmit()
 
@@ -35,6 +73,20 @@ function onSubmit(evt) {
 * */
 function validateForm(form) {
     var requiredFields = ['firstName', 'lastName', 'standing', 'age'];
+    var idx;
+    var formValid = true;
+
+    for (idx = 0; idx < requiredFields.length; ++idx) {
+        formValid &= validateRequiredField(form.elements[requiredFields[idx]]);
+    }
+
+    if (!formValid) {
+        var errormsg = document.getElementById('error-message');
+        errormsg.innerHTML = 'Fill out the required field';
+        errormsg.style.display = 'block';
+    }
+
+    return formValid;
 
 } //validateForm()
 
@@ -43,7 +95,15 @@ function validateForm(form) {
 * it will mark the field as invalid and return false. Otherwise it will return true.
 * */
 function validateRequiredField(field) {
-
+    var value = field.value.trim();
+    var valid = value.length > 0;
+    if (valid) {
+        field.className = 'form-control';
+    }
+    else {
+        field.className = 'form-control invalid-field';
+    }
+    return valid;
 } //validateRequiredField()
 
 document.addEventListener('DOMContentLoaded', onReady);
